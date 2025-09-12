af910157a27ffb2774b03dd4512c7897f98d814c
laiban219 <laiban7@nycstudents.net>
2025-09-12 14:05:46 +0000

    3b

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Laiba Nadeem"); 
+   System.out.println("Laiba Nadeem");
+   System.out.println("21927 74th Ave, Oakland Gardens,11364");
+   System.out.println("Laiba Nadeem \n 21927 74th Ave, Oakland Gardens,11364");
+  }
+
+  
+}
